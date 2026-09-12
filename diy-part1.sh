f01627133f599b5240785d951307ea354904c2ec
zhengshangui <80737250@qq.com>
2026-09-13 06:23:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,7 @@ git config --global --unset http.proxy
 git config --global --unset https.proxy
 
 # ghproxy加速镜像地址
-GHPROXY="https://mirror.ghproxy.com/"
+# GHPROXY="https://mirror.ghproxy.com/"
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 OPENWRT_DIR="${OPENWRT_DIR:-$(pwd)}"
