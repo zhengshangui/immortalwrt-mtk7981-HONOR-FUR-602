76419ed46e7db2f45138870a992827b8771eef23
zhengshangui <80737250@qq.com>
2026-09-13 15:57:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,53 +1,23 @@
 #!/bin/bash
-set -e -o pipefail
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 修复CI环境git弹窗账号询问报错128
-# git config --global core.askPass ""
-# git config --global credential.helper ""
-# git config --global --unset http.proxy
-# git config --global --unset https.proxy
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# ghproxy加速镜像地址
-# GHPROXY="https://mirror.ghproxy.com/"
-GHPROXY=""
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# ===== 删除这里 SCRIPT_DIR、OPENWRT_DIR、cd "${OPENWRT_DIR}" 全部删掉！=====
-
-UPDATE_PACKAGE() {
-	local pkg_name="$1"
-	local pkg_repo="$2"
-	local pkg_branch="$3"
-	local pkg_dir="$4"
-
-	rm -rf "package/${pkg_name}"
-	git clone --depth 1 --branch "${pkg_branch}" "${GHPROXY}https://github.com/${pkg_repo}.git" "package/${pkg_name}"
-	if [ -n "${pkg_dir}" ]; then
-		mv "package/${pkg_name}/${pkg_dir}"/* "package/${pkg_name}/"
-	fi
-	rm -rf "package/${pkg_name}/.git"
-	echo "fetched ${pkg_name}"
-}
-
-# ==========第三方插件拉取列表【全部注释，当前不下载插件】==========
-# OpenAppFilter
-# UPDATE_PACKAGE "luci-app-appfilter" "openwrt-develop/OpenAppFilter" "main" ""
-
-# MosDNS
-# UPDATE_PACKAGE "luci-app-mosdns" "sbwml/luci-app-mosdns" "v5" ""
-
-# netspeedtest 测速
-# UPDATE_PACKAGE "luci-app-netspeedtest" "sirpdboy/luci-app-netspeedtest" "main" ""
-
-# openlist2
-# UPDATE_PACKAGE "luci-app-openlist2" "sirpdboy/openlist2" "main" ""
-
-# partexp
-# UPDATE_PACKAGE "luci-app-partexp" "sirpdboy/luci-app-partexp" "main" ""
-
-# quickfile
-# UPDATE_PACKAGE "luci-app-quickfile" "sirpdboy/luci-app-quickfile" "main" ""
-
-# mwan3helper
-# UPDATE_PACKAGE "luci-app-mwan3helper" "sirpdboy/luci-app-mwan3helper" "main" ""
-
-echo "===== diy‑part1.sh finished，无插件下载 ====="
+# 添加第三方软件包
+#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/kenzok8/small-package package/small-package
+#git clone https://github.com/Zxilly/UA2F package/UA2F
