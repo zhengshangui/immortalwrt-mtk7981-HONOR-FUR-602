a6ee7c626807fbc419e429855eb1131c63b12624
zhengshangui <80737250@qq.com>
2026-09-15 06:51:48 +0800

    Update diy-360T7-hanwckf-1.sh

--- doc/diy/diy-360T7-hanwckf-1.sh
+++ doc/diy/diy-360T7-hanwckf-1.sh
@@ -1,22 +1,77 @@
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+#!/bin/bash
+set -e
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 增加git超时，避免github https 128错误
+git config --global http.lowSpeedLimit 1000
+git config --global http.lowSpeedTime 60
+git config --global core.compression 0
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 安装和更新软件包
+UPDATE_PACKAGE() {
+	local PKG_NAME=$1
+	local PKG_REPO=$2
+	local PKG_BRANCH=$3
+	local PKG_SPECIAL=$4
+	local PKG_LIST=("$PKG_NAME" $5)
+	local REPO_NAME=${PKG_REPO#*/}
 
-# 添加第三方软件包
-#git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-#git clone https://github.com/kenzok8/small-package package/small-package
-#git clone https://github.com/Zxilly/UA2F package/UA2F
+	echo " "
+	echo "Install plugin: $PKG_NAME"
 
+	for NAME in "${PKG_LIST[@]}"; do
+		echo "Search directory: $NAME"
+		local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
+
+		if [ -n "$FOUND_DIRS" ]; then
+			while read -r DIR; do
+				rm -rf "$DIR"
+				echo "Delete directory: $DIR"
+			done <<< "$FOUND_DIRS"
+		else
+			echo "Not found directory: $NAME"
+		fi
+	done
+
+	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+
+	if [[ "$PKG_SPECIAL" == "pkg" ]]; then
+		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
+		rm -rf ./$REPO_NAME/
+	elif [[ "$PKG_SPECIAL" == "name" ]]; then
+		mv -f $REPO_NAME $PKG_NAME
+	fi
+}
+
+# 拉取主题与插件
+UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
+UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
+UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
+UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
+UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
+
+UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
+UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
+UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
+UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
+UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
+UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
+UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
+UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
+UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
+UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
+
+# ====================================================================
+# 以下为第二段脚本内容（原本在 diy-360T7-padavanonly-2.sh 中）
+# 已删除 OpenSSL 降级补丁（因为 config 依赖 OpenSSL 3.4.0）
+# ====================================================================
+
+# 复制FUR-602 DTS
+if [ -f "../mt7981b-honor-fur-602.dts" ]; then
+    cp -f ../mt7981b-honor-fur-602.dts target/linux/mediatek/dts/mt7981b-honor-fur-602.dts
+fi
+
+# 更新软件包版本（可选）
+# UPDATE_VERSION "sing-box"
+# UPDATE_VERSION "tailscale"
+
+echo "DIY script finished."
